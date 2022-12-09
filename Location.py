769b8b6f587e44b7077bab50fc6df696cde616a8
Faysal Khatri <faysalkhatri@gmail.com>
2022-12-09 03:54:42 +0000

    loc and weather api drafts

---
+++ Location.py
@@ -0,0 +1,39 @@
+import requests
+import os
+from typing import Tuple
+
+class MissingOpenWeatherAPIKey(Exception):
+    """Open Weather API Key Missing. Expecting key in env var OPEN_WEATHER_API_KEY."""
+    pass
+
+class CouldNotFindLocation(Exception):
+    """Could not resolve location."""
+    pass
+
+class InvalidResponse(Exception):
+    """Invalid Response from Open Weather API."""
+    pass
+
+class GeoCoderAPI():
+    def __init__(self):
+        try:
+            self.apikey = os.environ["OPEN_WEATHER_API_KEY"]
+        except KeyError:
+            raise MissingOpenWeatherAPIKey
+        
+    def get_coordinates(self, loc: str) -> Tuple[str, str]:
+        url = "http://api.openweathermap.org/geo/1.0/direct"
+        params = {
+            "q": loc,
+            "limit": 1,
+            "appid": self.apikey
+        }
+        try:
+            r = requests.get(url, params).json()
+        except Exception:
+            raise InvalidResponse
+        try:
+            res = r[0]
+        except IndexError:
+            raise CouldNotFindLocation
+        return (res.get("lat"), res.get("lon"))
---
+++ Weather.py
@@ -0,0 +1,53 @@
+from GeoCoderAPI import MissingOpenWeatherAPIKey
+import datetime
+import requests
+import os
+from collections import namedtuple
+from typing import List
+
+Forecast = namedtuple("Forecast", 
+                     ["datetime",
+                      "description",
+                      "temp",
+                      "feels_like",
+                      "cloud_cover_pct",
+                      "precip_pct_chance",
+                      "wind_speed",
+                      "wind_gust",
+                      "visibility"
+                    ])
+
+class WeatherAPI():
+    def __init__(self):
+        try:
+            self.apikey = os.environ["OPEN_WEATHER_API_KEY"]
+        except KeyError:
+            raise MissingOpenWeatherAPIKey
+
+    def get_five_day_forecast(self, lat:str, lon:str) -> List[Forecast]:
+        url = "http://api.openweathermap.org/data/2.5/forecast"
+        params = {
+            "lat": lat,
+            "lon": lon,
+            "units": "imperial",
+            "appid": self.apikey
+        }
+
+        r = requests.get(url, params).json()
+
+        forecasts = []
+
+        for f in r.get("list"):
+            forecasts.append(Forecast(
+                datetime.datetime.fromtimestamp(f.get("dt")),
+                f.get("weather")[0].get("description"),
+                round(f.get("main").get("temp")),
+                round(f.get("main").get("feels_like")),
+                f.get("clouds").get("all"),
+                int(f.get("pop") * 100),
+                round(f.get("wind").get("speed")),
+                round(f.get("wind").get("gust")),
+                f.get("visibility")
+            ))
+            
+        return forecasts
