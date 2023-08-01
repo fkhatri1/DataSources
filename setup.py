b3be0a3e88c15238f2f03438d247f5fbe8f7ee8e
Faysal Khatri <faysalkhatri@gmail.com>
2023-08-01 16:53:20 +0000

    cleanup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name="OutsideWorld",
+        version="0.1",
+        packages=["OutsideWorld"],
+        license="MIT",
+        long_description=open("README.md").read(),
+    )
